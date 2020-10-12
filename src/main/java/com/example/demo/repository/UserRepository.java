be7ac6fc626f66e8b914942114ebde9cc3f97b9b
karikh <kostochkadub@gmail.com>
2020-10-12 18:04:10 +0300

    up

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -5,21 +5,23 @@ import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findAllByAgeGreaterThan(int minAge);
     List<User> findAllByAgeBetween(int minAge, int maxAge);
     List<User> deleteByName(String name);
 
-
-    @Transactional
     @Modifying
-    @Query(value = "INSERT INTO user (name, age, email) VALUES :name, :age, :email", nativeQuery = true)
-    List<User> addNewUser(String name, int age, String email);
+    @Query(value = "INSERT INTO user (name, age, email) VALUES (:name, :age, :email)", nativeQuery = true)
+    @Transactional
+    void addNewUser(@Param("name")String name,@Param("age") int age,@Param("email") String email);
 }
 
 
