6e9530b68f283a971c954314e9ec212d4137c498
karikh <kostochkadub@gmail.com>
2020-10-13 09:48:34 +0300

    up test addNotValidNewUser

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.repository;
 
-
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -15,13 +14,15 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findAllByAgeGreaterThan(int minAge);
+
     List<User> findAllByAgeBetween(int minAge, int maxAge);
+
     List<User> deleteByName(String name);
 
     @Modifying
     @Query(value = "INSERT INTO user (name, age, email) VALUES (:name, :age, :email)", nativeQuery = true)
     @Transactional
-    void addNewUser(@Param("name")String name,@Param("age") int age,@Param("email") String email);
+    void addNewUser(@Param("name") String name, @Param("age") int age, @Param("email") String email);
 }
 
 
