666d0d1093c478128f0f1a84e52b2992aab4ec61
karikh <kostochkadub@gmail.com>
2020-10-13 09:41:04 +0300

    up

--- src/test/java/com/example/demo/UserServiceTest.java
+++ src/test/java/com/example/demo/UserServiceTest.java
@@ -2,14 +2,12 @@ package com.example.demo;
 
 
 import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
